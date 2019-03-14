b1289a3523607c38086848f0361add2e48ac7e4a
adi <czech400m@gmail.com>
2019-03-14 23:21:24 +0100

    C_DefaultMethods solution commit

--- src/test/java/suite/JUnit4TestSuite.java
+++ src/test/java/suite/JUnit4TestSuite.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(value={
     exercises.A_Lambdas.class,
-//    exercises.B_Comparators.class,
-//    exercises.C_DefaultMethods.class,
+    exercises.B_Comparators.class,
+    exercises.C_DefaultMethods.class,
 //    exercises.D_SimpleStreams.class,
 //    exercises.E_IntermediateStreams.class,
 //    exercises.F_AdvancedStreams.class,
