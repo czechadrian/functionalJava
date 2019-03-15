12ea9be094be65af03a9f95ba82b8db38165dfca
adi <czech400m@gmail.com>
2019-03-16 00:13:12 +0100

    E_IntermediateStream solution commit + refactor other tests

--- src/test/java/suite/JUnit4TestSuite.java
+++ src/test/java/suite/JUnit4TestSuite.java
@@ -8,11 +8,11 @@ import org.junit.runners.Suite;
     exercises.A_Lambdas.class,
     exercises.B_Comparators.class,
     exercises.C_DefaultMethods.class,
-//    exercises.D_SimpleStreams.class,
-//    exercises.E_IntermediateStreams.class,
-//    exercises.F_AdvancedStreams.class,
-//    exercises.G_MatcherScanner.class,
-//    exercises.H_Challenges.class
+    exercises.D_SimpleStreams.class,
+    exercises.E_IntermediateStreams.class,
+    exercises.F_AdvancedStreams.class,
+    exercises.G_MatcherScanner.class,
+    exercises.H_Challenges.class
 })
 public class JUnit4TestSuite {
 }
